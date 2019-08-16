d900c5826770063d4b178b7c606db8a527fe86c9
katherinebl <katherine.briceno@kairosds.com>
2019-08-16 14:13:24 +0200

    Created add task functionality

---
+++ app.js
@@ -0,0 +1,50 @@
+// DEFINE UI VARS
+
+const taskInput = document.querySelector('#task');
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.colletion');
+const filter = document.querySelector('#filter');
+const clearBtn = document.querySelector('.clear-tasks');
+
+// CALL TO LOAD ALL EVENT LISTENERS
+loadAllEventListeners();
+
+//Load all event listeners
+function loadAllEventListeners() {
+    //Add task event
+    form.addEventListener('submit', addTask);
+}
+
+//Add task function
+function addTask(e) {
+    //Check if there's a value in the input
+    if (taskInput.value === '') {
+        alert('Add a task :)');
+    }
+
+    //Create item
+    const li = document.createElement('li');
+    //Asign class to item
+    li.className = 'colletion-item';
+    //Create text node with task content and append to item
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    //Create link
+    const link = document.createElement('a');
+    //Assing class to link
+    link.className = " delete-item secondary-content";
+    //Insert icon tag to link
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //Append link to item
+    li.appendChild(link);
+
+    //Appens item to the list
+    taskList.appendChild(li);
+
+    //Clear input
+    taskInput.value = "";
+
+    e.preventDefault();
+}
+
+
